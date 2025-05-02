6a6941317ccac7bb878d9632aec4da3f936cbf9e
ashutoshzxc01ofc <144043196+ashutoshzxc01ofc@users.noreply.github.com>
2025-05-02 09:55:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function MainContent() {
   return (
     <div className="flex-1 flex flex-col overflow-hidden x">
       <Header />
-      <div className="flex-1 overflow-y-auto p-6 bg-white">
+      <div className="flex-1 p-6 bg-white">
         <ScheduleSection />
         <div className="flex gap-6 ">
           <div className="flex-1 w-3/4">
